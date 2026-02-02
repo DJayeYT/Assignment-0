adafa6f55707947180348694d7bb10aaa4f5beb7
DJayeYT <djschlarmann@gmail.com>
2026-02-01 23:15:56 -0800

    Added new value of a and added comment

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int a = 10;
+        int a = 11;
         int b = 5;
 
+        // Print the sum of a and b
         System.out.println(a + b);
     }
 }
