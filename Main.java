d86ecc46e0f838c74ac7eb48a5e8beab98c4a6a5
DJayeYT <djschlarmann@gmail.com>
2026-02-01 23:21:49 -0800

    Changed value of a

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         // Initialize two integer variables
-        int a = 12;
+        int a = 13;
         int b = 5;
 
         // Print the sum of a and b
